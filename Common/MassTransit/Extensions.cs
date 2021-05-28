9f0895d78fb7ee1f15f980591c31b565eba9f729
Yogi <yogeshcs2003@gmail.com>
2021-05-28 18:59:18 +0530

    done

---
+++ Common/MassTransit/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using CommandCenter.Setting;
+using CommandCenter.Settings;
+using GreenPipes;
+using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumers(Assembly.GetEntryAssembly());
+                x.UsingRabbitMq((context, configurator) =>
+                {
+                    var configuration = context.GetService<IConfiguration>();
+                    var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                    var rabbitMqSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    configurator.Host(rabbitMqSettings.Host);
+                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+                    configurator.UseMessageRetry(b =>
+                    {
+                        b.Interval(3, TimeSpan.FromSeconds(5));
+                    });
+                });
+            });
+            services.AddMassTransitHostedService();
+            return services;
+        }
+    }
+}
